312d7a77a66bd82f19f3796e8d9e87c19e3fad74
centurion-crawler <22920182204393@stu.xmu.edu.cn>
2020-03-27 22:06:39 +0800

    3.27

--- truffle-config.js
+++ truffle-config.js
@@ -9,26 +9,18 @@ var HDWalletProvider = require("truffle-hdwallet-provider");
 var infura_apikey="bb74b607951d4e469dde2124c6320675";
 
 var mnemonic_ropsten=process.env.mnemonic_ropsten;
-var mnemonic_mainnet=process.env.mnemonic_mainnet;
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
    networks: {
-  //   development: {
-  //     host: "127.0.0.1",
-  //     port: 7545,
-  //     network_id: "*", // Match any network 
-  //     gas : 3000000000
-  //   },
-  //   local: {
-  //     host :"127.0.0.1",
-  //     port: 8545,
-  //     network_id:"*",
-  //     gas : 30000000000
-  //   }
-
+     development: {
+       host: "127.0.0.1",
+       port: 7545,
+       network_id: "*", // Match any network 
+       gas : 3000000000
+     },
     ropsten:{
-      provider :new HDWalletProvider(mnemonic_ropsten,"https://ropsten.infura.io/"+infura_apikey),
+      provider :new HDWalletProvider(mnemonic_ropsten,"https://mainnet.infura.io/v3/"+infura_apikey),
       network_id: 3,
       gas: 7003605,
       gasPrice: 100000000000,
