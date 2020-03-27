78f0e46a86df3e7513c5dfe701109b5bccbe0774
centurion-crawler <22920182204393@stu.xmu.edu.cn>
2020-03-27 20:24:27 +0800

    new 3.27

--- src/js/app.js
+++ src/js/app.js
@@ -27,7 +27,7 @@ App = {
     }
     // If no injected web3 instance is detected, fall back to Ganache
     else {
-      App.web3Provider = new Web3.providers.HttpProvider('http://192.168.1.115:7545');
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
     }
     web3 = new Web3(App.web3Provider);
 
--- truffle-config.js
+++ truffle-config.js
@@ -13,24 +13,26 @@ var mnemonic_mainnet=process.env.mnemonic_mainnet;
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*", // Match any network 
-      gas : 3000000000
-    },
-    local: {
-      host :"127.0.0.1",
-      port: 8545,
-      network_id:"*",
-      gas : 30000000000
-    },
+   networks: {
+  //   development: {
+  //     host: "127.0.0.1",
+  //     port: 7545,
+  //     network_id: "*", // Match any network 
+  //     gas : 3000000000
+  //   },
+  //   local: {
+  //     host :"127.0.0.1",
+  //     port: 8545,
+  //     network_id:"*",
+  //     gas : 30000000000
+  //   }
+
     ropsten:{
       provider :new HDWalletProvider(mnemonic_ropsten,"https://ropsten.infura.io/"+infura_apikey),
-      network_id : 3,
-      gas :300000000,
-      gasPrice: 3000000000000
+      network_id: 3,
+      gas: 7003605,
+      gasPrice: 100000000000,
+      networkCheckTimeout: 600000
     }
     // mainnet:{
     //   provider: function () {
